bb0f104d57fef32e6679e34aa5c8f117c78cb8a3
Matias-DR <Matias-DR@outlook.com>
2024-03-20 22:12:26 -0300

    ADD: root/general layout

---
+++ src/layouts/root-layout.tsx
@@ -0,0 +1,98 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import caritasLogo from 'public/caritas-logo.svg'
+
+import { User } from '@/types'
+
+export default function RootLayout({
+  user,
+  children
+}: Readonly<{
+  user: User
+  children: React.ReactNode
+}>) {
+  return <><header
+    className='font-bold'
+  >
+    <Image
+      alt='Logo Cáritas'
+      src={caritasLogo}
+      className='w-[3rem]'
+    />
+    <Link
+      key='link-home'
+      href='/'
+    >
+      Inicio
+    </Link>
+    <Link
+      key='link-we'
+      href='/we'
+    >
+      Nosotros
+    </Link>
+    {
+      user.role === 'non-registered' ? <>
+        <div
+          key='header-separator'
+          className='flex-grow'
+        />
+        <Link
+          key='link-signin'
+          href='/sign/in'
+        >
+          Iniciar Sesión
+        </Link>
+        <Link
+          key='link-signup'
+          href='/sign/up'
+        >
+          Registrarse
+        </Link>
+      </> :
+        user.role === 'Nominal' ? <>
+          <div
+            key='header-separator'
+            className='flex-grow'
+          />
+          <Link
+            key='link-user'
+            href='/user'
+          >
+            {user.name}
+          </Link>
+          <Link
+            key='link-signout'
+            href='/signout'
+          >
+            Cerrar Sesión
+          </Link>
+        </> : <>
+          <Link
+            key='link-statistics'
+            href='/statistics'
+          >
+            Estadísticas
+          </Link>
+          <div
+            key='header-separator'
+            className='flex-grow'
+          />
+          <Link
+            key='link-user'
+            href='/user'
+          >
+            {user.name}
+          </Link>
+          <Link
+            key='link-signout'
+            href='/signout'
+          >
+            Cerrar Sesión
+          </Link>
+        </>
+    }
+  </header>
+    {children}
+  </>
+}
