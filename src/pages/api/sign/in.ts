aa3a14f0443d38004797cb78ed71d6ff99f48689
Matias-DR <Matias-DR@outlook.com>
2024-03-20 22:13:09 -0300

    ADD: endpoint in nextjs server for signin

---
+++ src/pages/api/sign/in.ts
@@ -0,0 +1,44 @@
+import type {
+  NextApiRequest,
+  NextApiResponse
+} from 'next'
+
+import axios from 'axios'
+
+import { BACK_BASE_URL } from '@/constants'
+import { setToken } from '@/utils'
+
+/**
+ * Async handler function that sends the signin form data to the external server.
+ * Filters the response and retrieves the appropriate status code and message
+ * @arg {NextApiRequest} req
+ * @arg {NextApiResponse} res
+ */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> {
+  const formData = req.body
+
+  await axios
+    .post(`${BACK_BASE_URL}sign/up/`, formData)
+    .then((result: any) => {
+      setToken(
+        'access',
+        result.data.access,
+        req,
+        res,
+        // new Date(new Date().setSeconds(new Date().getSeconds() + 3))
+      )
+      res.status(result.status).json({})
+    },
+    )
+    .catch((result: any) => {
+      try {
+        res.status(result.status).json({ message: result.data.message })
+      } catch {
+        res.status(500).json({ message: 'Ah ocurrido un error inesperado.' })
+      }
+    },
+    )
+}
