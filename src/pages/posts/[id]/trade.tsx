95a8af32c98316bf14a60f1756b134518b2749a2
Matias Diz Rendani <Matias-DR@outlook.com>
2024-05-25 00:39:08 -0300

    ADD: permissions

--- src/pages/posts/[id]/trade.tsx
+++ src/pages/posts/[id]/trade.tsx
@@ -1,4 +1,17 @@
+import { GetSSPropsResult } from '@/types'
+import { getUser, requirePermission } from '@/utils'
+import { NextApiRequest, NextApiResponse } from 'next'
 import { useRouter } from 'next/router'
+
+export async function getServerSideProps ({
+  req,
+  res
+}: Readonly<{
+  req: NextApiRequest
+  res: NextApiResponse
+}>): Promise<GetSSPropsResult> {
+  return requirePermission(getUser(req, res), 'usuario_basico')
+}
 
 export default function Trade () {
   const router = useRouter()
