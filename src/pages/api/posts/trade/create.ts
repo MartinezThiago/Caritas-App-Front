93a7d8ae8fff9dcddb310ecdea29ed9cf12b28f0
Matias Diz Rendani <Matias-DR@outlook.com>
2024-05-25 17:02:54 -0300

    feat: Add API endpoint for creating trades

---
+++ src/pages/api/posts/trade/create.ts
@@ -0,0 +1,31 @@
+import { BACK_BASE_URL } from '@/constants'
+import axios from 'axios'
+import { getCookie } from 'cookies-next'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  // ADAPTAR SI ES EL CASO
+  const form = {
+    productos_de_oferta: req.body.offer,
+    centro_elegido: req.body.center,
+    dia_elegido: req.body.day,
+    hora_elegida: req.body.hour
+  }
+  const token = getCookie('access', { req, res })
+  const config = { headers: { Authorization: `Bearer ${token}` } }
+  await axios
+    .post(`${BACK_BASE_URL}<URL>`, form, config)
+    .then((result: any) => {
+      res.status(result.status).json(result.data)
+    })
+    .catch((result: any) => {
+      try {
+        res.status(result.status).json({ message: result.data.message })
+      } catch {
+        res.status(500).json({ message: 'Ha ocurrido un error inesperado.' })
+      }
+    })
+}
