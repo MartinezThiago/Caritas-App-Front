e2cba61aa1a0f05aba8d480b937d5829a28c5dc0
Martinez Thiago <thiagomartinez1240@gmail.com>
2024-05-25 10:09:49 -0300

    FAVS fixed

--- src/components/extended-post-card.tsx
+++ src/components/extended-post-card.tsx
@@ -60,20 +60,22 @@ export default function ExtendedPostCard(props: PostData) {
   }, []);
 
   useEffect(() => {
-    const getIdsPostFavs = async () => {
-      await axios
-        .get<any[]>(`${FRONT_BASE_URL}/user/favs/getIdFavs`)
-        .then((res: any) => {
-          console.log(res.data);
-          setPostFavsUser(res.data)
-          setSavedPost(!res.data.includes(props.idPost))
+    if (props.user.role == 'usuario_basico') {
+      const getIdsPostFavs = async () => {
+        await axios
+          .get<any[]>(`${FRONT_BASE_URL}/user/favs/getIdFavs`)
+          .then((res: any) => {
+            console.log(res.data);
+            setPostFavsUser(res.data)
+            setSavedPost(!res.data.includes(props.idPost))
 
-        })
-        .catch((err: any) => {
-          setPostFavsUser([])
-        })
+          })
+          .catch((err: any) => {
+            setPostFavsUser([])
+          })
+      }
+      getIdsPostFavs();
     }
-    getIdsPostFavs();
   }, []);
 
 
