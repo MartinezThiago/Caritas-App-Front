10c7f5c56cd8e8bc9a528f817aa8930272015ef6
Matias Diz Rendani <Matias-DR@outlook.com>
2024-05-27 01:58:14 -0300

    correcciones: filtrado categoria de productos a ofertar, agregado de data en form, fecha vía date input

--- src/pages/posts/[id]/trade.tsx
+++ src/pages/posts/[id]/trade.tsx
@@ -1,9 +1,10 @@
 import { CardProduct, MultiSelect } from '@/components'
-import { FRONT_BASE_URL } from '@/constants'
+import { FRONT_BASE_URL, daysByDay } from '@/constants'
 import { RootLayout } from '@/layouts'
 import { GetSSPropsResult, PostDataAdapter, User } from '@/types'
-import { getUser, requirePermission, makeTimeRange } from '@/utils'
+import { getUser, makeTimeRange, requirePermission } from '@/utils'
 import axios from 'axios'
+import dayjs from 'dayjs'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { useRouter } from 'next/router'
 import auxPic from 'public/post-image-preview.jpg'
@@ -24,27 +25,56 @@ interface Option {
   label: string
 }
 
+const validateDay = (day: string, days: string[]): boolean => {
+  const dayOfWeek = dayjs(day).day()
+  const isDayOfWeekAvailable = days.includes(daysByDay[dayOfWeek])
+  if (isDayOfWeekAvailable) {
+    return true
+  } else {
+    return false
+  }
+}
+
 const Field = ({
   text,
   options,
-  handleClick
+  handleClick,
+  datepicker = false,
+  datePickerValue,
+  datepickerText = ''
 }: {
   text: string
-  options: Array<{ value: string; label: string }>
+  options?: Array<{ value: string; label: string }>
   handleClick: (value: string) => void
+  datepicker?: boolean
+  datePickerValue?: string
+  datepickerText?: string
 }): React.ReactNode => {
   return (
     <div className='w-full max-w-96 flex flex-col'>
       <h1 className='w-full text-center text-xl'>{text}</h1>
       <div className='w-full max-w-96 p-4'>
-        <MultiSelect
-          props={{
-            options,
-            setValue: (_: string, value: string) => {
-              handleClick(value)
-            }
-          }}
-        />
+        {datepicker ? (
+          <>
+            <input
+              type='date'
+              className='w-full h-[2.5rem] mt-4 py-2 px-3 appearance-none border shadow text-zinc-700 focus:outline-none focus:shadow-lg border-zinc-200 bg-white hover:bg-zinc-100 hover:text-zinc-900 dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50'
+              onChange={(e: any) => handleClick(e.target.value)}
+              min={new Date().toISOString().split('T')[0]}
+              value={datePickerValue}
+            />
+            <p className='mt-2 mb-4'>{datepickerText}</p>
+          </>
+        ) : (
+          <MultiSelect
+            props={{
+              options,
+              setValue: (_: string, value: string) => {
+                handleClick(value)
+              }
+            }}
+          />
+        )}
       </div>
     </div>
   )
@@ -54,44 +84,31 @@ export default function Trade ({ user }: { user: User }) {
   const router = useRouter()
   const [postData, setPostData] = useState<PostDataAdapter | undefined>()
   const [posts, setPosts] = useState<[] | undefined>()
-  const [postsSelected, setPostsSelected] = useState<string[]>([])
+  const [postSelected, setPostSelected] = useState<string | undefined>()
   const [centers, setCenters] = useState<Option[]>([])
-  const [center, setCenter] = useState<string>('')
-  const [days, setDays] = useState<Record<string, Option[]>>({})
+  const [center, setCenter] = useState<string | undefined>(undefined)
+  const [days, setDays] = useState<string[]>([])
   const [day, setDay] = useState<string>('')
   const [hours, setHours] = useState<Record<string, Option[]>>({})
-  const [hour, setHour] = useState<string>('')
+  const [hour, setHour] = useState<string | undefined>(undefined)
 
   useEffect(() => {
-    const getPosts = async () => {
-      await axios
-        .get(`${FRONT_BASE_URL}user/getposts`)
-        .then((res: any) => {
-          setPosts(res.data)
-        })
-        .catch((res: any) => {
-          setPosts([])
-        })
-    }
-    const getPostData = async () => {
+    const getData = async () => {
       await axios
         .post<PostDataAdapter>(`${FRONT_BASE_URL}post/get`, {
           id: router.query.id
         })
-        .then((res: any) => {
-          setPostData(res.data)
+        .then((postRes: any) => {
+          setPostData(postRes.data)
           const centerOptions: Option[] = []
-          const dayOptions: Record<string, Option[]> = {}
+          let dayOptions: string[] = []
           const hourOptions: Record<string, Option[]> = {}
-          res.data.centros.forEach((center: any) => {
+          postRes.data.centros.forEach((center: any) => {
             centerOptions.push({
               value: center.id_centro,
               label: `${center.ubicacion} | ${center.direccion} | ${center.nombre_centro}`
             })
-            dayOptions[center.id_centro] = center.dias.map((day: any) => ({
-              value: day.idDia,
-              label: day.descripcion
-            }))
+            dayOptions = center.dias.map((day: any) => day.descripcion)
             hourOptions[center.id_centro] = makeTimeRange(
               center.horario_apertura,
               center.horario_cierre
@@ -103,20 +120,40 @@ export default function Trade ({ user }: { user: User }) {
           setCenters(centerOptions)
           setDays(dayOptions)
           setHours(hourOptions)
+
+          const getPosts = async () => {
+            await axios
+              .get(`${FRONT_BASE_URL}user/getposts`)
+              .then((postsRes: any) => {
+                try {
+                  const filteredPosts = postsRes.data.filter(
+                    (post: any) =>
+                      post.categoria_producto ===
+                      postRes.data.categoria_producto
+                  )
+                  setPosts(filteredPosts.length > 0 ? filteredPosts : undefined)
+                } catch {
+                  setPosts(undefined)
+                }
+              })
+              .catch((postsRes: any) => {
+                setPosts(undefined)
+              })
+          }
+          getPosts()
         })
         .catch((err: any) => {})
     }
-    getPosts()
-    getPostData()
+    getData()
   }, [])
 
   const handlePostClick = (id: string) => {
-    if (postsSelected.includes(id)) {
-      setPostsSelected(prev => prev.filter(prevId => prevId !== id))
-    } else {
-      setPostsSelected(prev => [...prev, id])
+    let newPostSelected = undefined
+    if (postSelected !== id) {
+      newPostSelected = id
     }
-    if (postsSelected.length === 0) {
+    setPostSelected(newPostSelected)
+    if (!newPostSelected) {
       setCenter('')
       setDay('')
       setHour('')
@@ -127,8 +164,14 @@ export default function Trade ({ user }: { user: User }) {
     setCenter(center)
   }
 
-  const handleDayClick = (day: string) => {
-    setDay(day)
+  const handleDayClick = (value: string) => {
+    let newDay = ''
+    if (validateDay(value, days)) {
+      newDay = value
+    } else {
+      alert('El día seleccionado no se encuentra disponible')
+    }
+    setDay(newDay)
   }
 
   const handleHourClick = (hour: string) => {
@@ -137,7 +180,9 @@ export default function Trade ({ user }: { user: User }) {
 
   const handleSubmit = async () => {
     const form = {
-      offer: postsSelected,
+      post: router.query.id,
+      offer: postSelected,
+      'bidding-user-id': user.userId,
       center,
       day,
       hour
@@ -162,45 +207,54 @@ export default function Trade ({ user }: { user: User }) {
           {posts ? (
             <>
               <h1 className='w-full text-center'>
-                1) Seleccione los productos a ofertar/intercambiar
+                1) Seleccione el producto a ofertar/intercambiar
               </h1>
               <div className='w-full p-4 flex justify-center items-center overflow-x-auto'>
-                {posts.map((post: any, index: number) => (
-                  <>
-                    <CardProduct
-                      key={post.id}
-                      id={post.id}
-                      title={post.titulo}
-                      desciption={post.descripcion}
-                      nameProductCategorie={post.nombre_categoria_producto}
-                      nameProductState={post.nombre_estado_producto}
-                      locationTrade={post.ubicacion_trade}
-                      image={
-                        post.imagenes[0].base64_imagen
-                          ? post.imagenes[0].base64_imagen
-                          : auxPic
-                      }
-                      ownerPost={
-                        post.usuario_owner === parseInt(user.userId.toString())
-                      }
-                      handleClick={handlePostClick}
-                      enableLink={false}
-                    />
-                    {postsSelected.includes(post.id.toString()) && (
-                      <div className='w-0 h-0 p-0 m-0 relative'>
-                        <p className='pb-2 px-2 absolute bottom-24 right-6 text-6xl bg-blue-700 rounded-full text-white'>
-                          ✰
-                        </p>
-                      </div>
-                    )}
-                  </>
-                ))}
+                {posts.map((post: any) => {
+                  if (
+                    postSelected !== post.id.toString() &&
+                    postSelected !== undefined
+                  ) {
+                    return null
+                  }
+                  return (
+                    <>
+                      <CardProduct
+                        key={post.id}
+                        id={post.id}
+                        title={post.titulo}
+                        desciption={post.descripcion}
+                        nameProductCategorie={post.nombre_categoria_producto}
+                        nameProductState={post.nombre_estado_producto}
+                        locationTrade={post.ubicacion_trade}
+                        image={
+                          post.imagenes[0].base64_imagen
+                            ? post.imagenes[0].base64_imagen
+                            : auxPic
+                        }
+                        ownerPost={
+                          post.usuario_owner ===
+                          parseInt(user.userId.toString())
+                        }
+                        handleClick={handlePostClick}
+                        enableLink={false}
+                      />
+                      {postSelected === post.id.toString() && (
+                        <div className='w-0 h-0 p-0 m-0 relative'>
+                          <p className='pb-2 px-2 absolute bottom-24 right-6 text-6xl bg-blue-700 rounded-full text-white'>
+                            ✰
+                          </p>
+                        </div>
+                      )}
+                    </>
+                  )
+                })}
               </div>
             </>
           ) : (
             <h1>No posee productos de igual tipo para intercambiar</h1>
           )}
-          {postsSelected.length > 0 && (
+          {postSelected && (
             <>
               <div className='w-full flex justify-center items-center'>
                 <Field
@@ -208,14 +262,18 @@ export default function Trade ({ user }: { user: User }) {
                   options={centers}
                   handleClick={handleCenterClick}
                 />
-                {postsSelected.length > 0 && center ? (
+                {postSelected && center ? (
                   <Field
                     text='3) Seleccione el día'
-                    options={days[center]!}
                     handleClick={handleDayClick}
+                    datepicker
+                    datePickerValue={day}
+                    datepickerText={`Solo se encuentra los días ${days.join(
+                      ', '
+                    )}`}
                   />
                 ) : null}
-                {postsSelected.length > 0 && center && day ? (
+                {postSelected && center && day ? (
                   <Field
                     text='4) Seleccione el horario'
                     options={hours[center]!}
@@ -223,7 +281,7 @@ export default function Trade ({ user }: { user: User }) {
                   />
                 ) : null}
               </div>
-              {postsSelected.length > 0 && center && day && hour ? (
+              {postSelected && center && day && hour ? (
                 <div className='w-full flex justify-center items-center pb-4'>
                   <button
                     onClick={handleSubmit}
